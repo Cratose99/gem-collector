e61dd72227884e8fbe70876df44793beb6175603
Cratose99 <gabriel.eyer007@gmail.com>
2019-09-04 22:46:38 -0700

    made major improvments in implementing the total score and counters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,57 @@
-var winsCounter = 0;
-var randomNum = 0;
-var randomString = Math.floor(Math.random() * 11) + 1;
 
+
+
+
+// GLOBAL VARIABLES
+var winsCounter = 0;
+var losesCounter = 0;
+var targetNum = 0; // number to guess TODO: create div to display value
+var valueCounter = 0; 
+
+// FUNCTIONS
+function getRandomNum() {
+  return Math.floor(Math.random() * 11)+1;
+}
+
+
+// METHODS
 $(document).ready(function() {
-  $("#btn1").click(function() {
-    $("#gem1Value").append(randomString);
+  $('#valueCounter').text(valueCounter)
+  // SET UP GAME
+  // 1. Generate a numberToGuess (targetNumber)
+  // 2. Initialize a starting valueCounter
+  // 3. Assign each crustal a random value between 1 - 12
+  
+  //  PLAY GAME
+  // 1. Player clicks crystal
+  // 2. Grab value of crystal  (using jQuery)
+  // 3. Add value of crystal to valueCounter
+  // 4. If  valueCounter = trgetNumber ---> wins++  **
+  // else if valueCounter > targetNumber -->> losses++ **
+  
+  let random1 = getRandomNum()
+ $("#btn1").attr('value', random1) 
+
+ let random2 = getRandomNum()
+ $("#btn2").attr('value', random2) 
+
+ let random3 = getRandomNum()
+ $("#btn3").attr('value', random3) 
+ 
+ let random4 = getRandomNum()
+ $("#btn4").attr('value', random4) 
+
+  $(".diamond").on('click', function() { 
+    let value = parseInt($(this).attr('value'));
+    // console.log(value);
+   valueCounter += value  
+    // console.log('>>>>>>>>>>', valueCounter);
+    $('#valueCounter').text(valueCounter)
+    
   });
-});
+  
+  
+
+
+})
 
